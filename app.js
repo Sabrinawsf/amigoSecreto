9d557ed9ec1872b8f75159a0dda55c5bdcf17164
Sabrinawsf <sabrina.sf@sempreceub.com>
2025-09-02 18:53:44 -0300

    Atualizar README.md com descrição completa do projeto

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
 // Array que vai guardar os nomes dos participantes
-// Array de amigos
 let amigos = [];
 
 // Função para atualizar a lista na tela
